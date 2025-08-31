9ee84f96558420f4a8952625a0d07705b60213a7
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-08-31 18:00:33 -0500

    Added units in Movement

--- src/components/consumption-view/Movement.jsx
+++ src/components/consumption-view/Movement.jsx
@@ -1,8 +1,8 @@
-export function Movement({ name, value }) {
+export function Movement({ name, value, units }) {
   return (
     <div className="inline-flex flex-col items-start justify-start border-gray-700 border-2 text-gray-700 rounded-2xl p-2 bg-gray-200 shadow-sm max-w-max">
       <p className="text-xs font-medium">{name}</p>
-      <p className="mt-1 text-sm font-semibold">{value}</p>
+      <p className="mt-1 text-sm font-semibold">{value} {units}</p>
     </div>
   );
 }
